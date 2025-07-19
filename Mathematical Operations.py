ff64c5a7af64a54fabcf8ff24d9ae7685330cbdf
NyxCiph3r <151660576+PARIKAKUGOD1@users.noreply.github.com>
2025-07-19 15:57:13 +0530

    Add files via upload

---
+++ Mathematical Operations.py
@@ -0,0 +1,17 @@
+first_num=input("Enter first number: ")
+first_num=int(first_num)
+second_num=input("Enter second number: ")
+second_num=int(second_num)
+
+addition=first_num+second_num
+
+subtraction=first_num-second_num
+
+multiplication=first_num*second_num
+
+division=first_num/second_num
+
+print("Addition is: " , addition)
+print("Subtraction is: " , subtraction)
+print("Multiplication is: " , multiplication)
+print("Division is: " , division)
